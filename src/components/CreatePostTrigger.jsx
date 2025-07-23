9ed066eca3458f2bf73bacc5bd99e06534fed5a2
Michael George Alfred <Michoolgeorge@gmail.com>
2025-07-23 11:14:08 +0300

    Update UI of CreatePostTrigger.jsx

--- src/components/CreatePostTrigger.jsx
+++ src/components/CreatePostTrigger.jsx
@@ -3,7 +3,7 @@ import PhotoName from "./PhotoName";
 
 export default function CreatePostTrigger({ onClick, currentUser }) {
   return (
-    <div className="w-full max-w-3xl mx-auto flex gap-1 rounded-xl p-4 shadow-sm border-2 border-blue-700">
+    <div className="w-full max-w-3xl mx-auto flex gap-1 rounded-xl p-4 shadow-sm bg-blue-50">
       <NavLink to={`/profile/${currentUser?.uid}`}>
         {currentUser.photoURL ? (
           <img
@@ -19,7 +19,7 @@ export default function CreatePostTrigger({ onClick, currentUser }) {
       </NavLink>
       <button
         onClick={onClick}
-        className="w-full flex items-center gap-2 px-3 md:px-4 py-3 md:py-3 bg-gray-100 text-gray-700 hover:bg-gray-200 text-sm md:text-base rounded-full cursor-pointer>
+        className="w-full flex items-center gap-2 px-3 md:px-4 py-3 md:py-3 bg-blue-100 text-blue-700 hover:bg-blue-200 text-sm md:text-base rounded-full cursor-pointer>
         ">
         {`What's on your mind, ${currentUser?.displayName || "user"}?`}
       </button>
